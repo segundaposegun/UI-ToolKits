db290bb94030ee6d51ed9d446a2e9b7c14eb1f82
segundaposegun <segundaposegun@gmail.com>
2026-02-08 14:12:20 +0100

    Add files via upload

---
+++ gridFeatures/app/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+import FeaturesSectionDemo from "../components/features-section-demo-2"
+
+export default function SyntheticV0PageForDeployment() {
+  return <FeaturesSectionDemo />
+}
